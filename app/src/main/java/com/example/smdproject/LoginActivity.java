e6319855c370844c1466f864f5f8424eb36d63ac
Usman Faisal <usmanfaisal49@gmail.com>
2024-05-06 04:57:13 +0500

    Login Auth

--- app/src/main/java/com/example/smdproject/LoginActivity.java
+++ app/src/main/java/com/example/smdproject/LoginActivity.java
@@ -1,2 +1,151 @@
-package com.example.smdproject;public class LoginActivity {
+package com.example.smdproject;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+    EditText etemail;
+    EditText etpassword;
+
+    Button btnLogin;
+    Button btnGoogleLogin;
+
+    TextView tvforgotPassword;
+    TextView tvsignUpRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+        setButtonClickListener();
+    }
+
+    void init()
+    {
+        auth = FirebaseAuth.getInstance();
+        etemail = findViewById(R.id.et_email);
+        etpassword= findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvforgotPassword = findViewById(R.id.tv_forgot_password);
+        tvsignUpRedirect = findViewById(R.id.tv_signup_redirect);
+    }
+
+    void setButtonClickListener()
+    {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginValidation();
+            }
+        });
+
+        tvsignUpRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+
+        tvforgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forgotPasswordModal();
+            }
+        });
+    }
+
+    void forgotPasswordModal()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot, null);
+        EditText emailBox = dialogView.findViewById(R.id.emailBox);
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+        dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = emailBox.getText().toString();
+                if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                    Toast.makeText(LoginActivity.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Check your email", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        if (dialog.getWindow() != null){
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        dialog.show();
+    }
+
+
+    void loginValidation()
+    {
+        String email = etemail.getText().toString();
+        String pass = etpassword.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (!pass.isEmpty()) {
+                auth.signInWithEmailAndPassword(email, pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } else {
+                etpassword.setError("Empty fields are not allowed");
+            }
+        } else if (email.isEmpty()) {
+            etemail.setError("Empty fields are not allowed");
+        } else {
+            etemail.setError("Please enter correct email");
+        }
+    }
 }
