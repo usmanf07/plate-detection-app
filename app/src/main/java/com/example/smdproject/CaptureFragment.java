8a4cec9a26e43bd7f12d30784fcae6559bac6bbb
Nisa Nadeem <nisa.nadeem@dubizzlelabs.com>
2024-05-11 21:39:16 +0500

    work done!!!

--- app/src/main/java/com/example/smdproject/CaptureFragment.java
+++ app/src/main/java/com/example/smdproject/CaptureFragment.java
@@ -1,4 +1,22 @@
 package com.example.smdproject;
 
-public class CaptureFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CaptureFragment extends Fragment {
+
+    public CaptureFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_capture, container, false);
+    }
 }
--- app/src/main/java/com/example/smdproject/HomeActivity.java
+++ app/src/main/java/com/example/smdproject/HomeActivity.java
@@ -1,4 +1,49 @@
 package com.example.smdproject;
 
-public class HomeActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private FrameLayout fragmentContainer;
+    private ImageButton btnHistoryScans, btnCapture, btnProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        fragmentContainer = findViewById(R.id.fragment_container);
+        btnHistoryScans = findViewById(R.id.btn_history_scans);
+        btnCapture = findViewById(R.id.btn_capture);
+        btnProfile = findViewById(R.id.btn_profile);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
+
+
+        btnHistoryScans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
+            }
+        });
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CaptureFragment()).commit();
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+            }
+        });
+    }
 }
